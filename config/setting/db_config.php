987667114b406e72a2c5ee5d0754bb2e1be8963d
zinalpatel16 <zinal.patel@horizoncore.com>
2023-03-06 13:26:05 +0530

    Update db_config.php

--- config/setting/db_config.php
+++ config/setting/db_config.php
@@ -3,7 +3,8 @@
 $server = "54.160.30.200:5432";
 $user_name = "postgres";
 $password = "sdmySL58DHVerp1";
-$database = "ecommunity_fiber_base_db";
+//$database = "ecommunity_fiber_base_db";
+$database = "ecommunity_fiber_base_db_23_2_2023";
 # ------------------------------------
 
 ?>
